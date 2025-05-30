aade413be8f8010b2a1b5f847ada66ee56a1be7d
Busra Mestan <bmestan50@gmail.com>
2025-05-30 13:43:58 +0300

    Add encapsulation with getters and setters

--- src/Car.java
+++ src/Car.java
@@ -1,13 +1,48 @@
 public class Car {
 
-    String marka;
-    String model;
-    int year;
+//    String brand;
+//    String model;
+//    int year;
 
-    // Constructor
-    public Car(String marka, String model, int year) {
-        this.marka = marka;
+    /*
+    Constructor
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
         this.model = model;
+        this.year = year;
+    }
+     */
+    private String brand;
+    private String model;
+    private int year;
+
+
+    // Getter ve Setter methods
+
+    // brand
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    // model
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    // year
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
         this.year = year;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,33 @@ public class Main {
     public static void main(String[] args) {
 
         //Constructor ile nesne oluşturma
-        Car myCar = new Car("Volskwagen","Passat", 2025);
+        //Car myCar = new Car("Volskwagen","Passat", 2025);
 
 
-//        myCar.marka="Honda";
+//        myCar.brand="Honda";
 //        myCar.model="Civic";
 //        myCar.year=2025;
 
-        System.out.println("Marka: " + myCar.marka);
+       /* System.out.println("Marka: " + myCar.brand);
         System.out.println("Model: " + myCar.model);
         System.out.println("Year: " + myCar.year);
+       */
+
+
+
+        Car myCar = new Car();  //Car nesnesi oluşturma
+
+       //Nesne oluşturduktan sonra setter metotları ile verileri ata
+
+        myCar.setBrand("Ford");
+        myCar.setModel("Focus");
+        myCar.setYear(2019);
+
+        // get metotları ile verileri ekrana yazdır
+        System.out.println("Araba Bilgileri:");
+        System.out.println("Marka: " + myCar.getBrand());
+        System.out.println("Model: " + myCar.getModel());
+        System.out.println("Yıl: " + myCar.getYear());
 
     }
 }
